1c835445df4203b24ec643692656439c5b24934c
omerA <oatar717@gmail.com>
2022-04-15 15:21:29 +0300

    dummy main

---
+++ UI/Main.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Game_Logic;
+
+namespace UI
+{
+    internal class Test
+    {
+        public static void Main(string[] args)
+        {
+            Game game = new (10);
+            game.PrintBoard();
+            Console.ReadLine();
+        }
+    }
+}
